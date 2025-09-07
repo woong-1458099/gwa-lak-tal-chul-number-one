65f4fdef17cb80127590c8a55f189aac30ae2e87
최연웅 <yunwoong2@naver.com>
2025-09-07 15:21:13 +0900

    DAY -3

--- DAY-3/growing carrot.py
+++ DAY-3/growing carrot.py
@@ -7,19 +7,29 @@
 T = int(input())
 
 for tc in range(1, T+1):
-    N = int(input())  # 당근 개수
-    carrots = list(map(int, input().split()))  # 당근 크기 리스트 !!!!
+    N = int(input())  # 당근 개수 ( 정수로 나타내야 되서 int )
+    carrots = list(map(int,input().split())) # 당근 크기 리스트
 
-    max_len = 1  # 최소 길이 1
-    cur_len = 1  # 연속으로 증가하는 구간 길이 
 
-    for i in range(1, N):  #range(start, stop)
-        if carrots[i] > carrots[i -1]:  # carrots[i] -> 현재 위치 당근 크기 , carrots[i-1] -> 바로 이전 당근 크기 
-            # 현재 당근 크기가 이전 당근 크기보다 크면이라는 뜻인데 연속 증가는 바로 이전 원소보다 커야 한다는 뜻
-            cur_len += 1
-            if cur_len > max_len:
-                max_len = cur_len
+
+   
+
+    max_len = 1  # 연속으로 커지지 않는 경우 구간의 최소 길이는 1이다
+                 # 만약 max_len을 0으로 두고 시작하면 최소 길이가 "0"이 나오게 된다.
+    current_len = 1  # 연속으로 증가하는 구간 길이 
+
+    for i in range(1, N):
+        if carrots[i] > carrots[i-1]:
+
+      #range(start, stop)
+          # carrots[i] -> 현재 위치 당근 크기 , carrots[i-1] -> 바로 이전 당근 크기 
+            # 현재 당근 크기가 이전 당근 크기보다 크면이라는 뜻인데 연속 증가를 의미
+            
+            current_len += 1
+            if current_len > max_len:
+                max_len = current_len
+
         else:
-            cur_len = 1
-    
+            current_len = 1
+
     print(f"#{tc} {max_len}")
